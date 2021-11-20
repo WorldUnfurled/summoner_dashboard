bc9dd7c08f180d906c1b1e4f2aa8ff3192c5e8a5
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-11-20 12:09:02 -0500

    Add wireframe for sidebar info boxes

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+const header = document.getElementById("header");
 const api_key = "RGAPI-a073e1a7-9652-4c89-83d2-de84757be48b";
 
-// Summoner by name
-
 fetch(
+  // Summoner by name
   `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Self%20and%20Sea?api_key=${api_key}`
 )
   .then((res) => res.json())
   .then((data) =>
     fetch(
+      // Entries by ID (Endpoint)
       `https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/${data.id}?api_key=${api_key}`
     )
       .then((res) => res.json())
-      .then((data) => console.log(data[0]))
+      .then((data) => (header.textContent = data[0].tier))
   );
-
-// Entries by ID (Endpoint)
