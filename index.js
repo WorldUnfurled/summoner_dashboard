cabd49dff64422799d9607a016f4fef985802743
Brooks Gunn <brooks.a.gunn@gmail.com>
2021-12-27 23:58:56 -0500

    Add matches by puuid fetch

--- index.js
+++ index.js
@@ -33,9 +33,17 @@ fetch(
             Math.round((data[0].wins / (data[0].wins + data[0].losses)) * 100) +
             "%)") &&
           (rankImg.src =
-            "img/ranks/" + data[0].tier + "_" + data[0].rank + ".png") &&
-          console.log(data)
+            "img/ranks/" + data[0].tier + "_" + data[0].rank + ".png")
       )
   );
 
-fetch();
+fetch(
+  // Summoner by name
+  `https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/Self%20and%20Sea?api_key=${api_key}`
+)
+  .then((res) => res.json())
+  .then((data) =>
+    fetch(
+      `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${data.puuid}/ids?start=0&count=20&api_key=RGAPI-14309d6e-6e49-4b0f-8280-282d64033ee2`
+    )
+  );
