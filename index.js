93a9f962bae2bec172c769986eda6ac7c66925b2
Brooks Gunn <brooks.a.gunn@gmail.com>
2022-01-01 23:24:43 -0500

    Update api key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const rankInfo = document.getElementById("rank-info");
 const rankWR = document.getElementById("rank-wr");
 const rankImg = document.getElementById("rank-image");
-const api_key = "RGAPI-14309d6e-6e49-4b0f-8280-282d64033ee2";
+const api_key = "RGAPI-9e105af1-a5ea-440c-b390-2a877e055c4e";
 
 fetch(
   // Summoner by name
@@ -44,6 +44,14 @@ fetch(
   .then((res) => res.json())
   .then((data) =>
     fetch(
-      `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${data.puuid}/ids?start=0&count=20&api_key=RGAPI-14309d6e-6e49-4b0f-8280-282d64033ee2`
+      `https://americas.api.riotgames.com/lol/match/v5/matches/by-puuid/${data.puuid}/ids?start=0&count=20&api_key=${api_key}`
     )
+      .then((res) => res.json())
+      .then((data) =>
+        fetch(
+          `https://americas.api.riotgames.com/lol/match/v5/matches/NA1_4154793253?api_key=${api_key}`
+        )
+          .then((res) => res.json())
+          .then((data) => console.log(data))
+      )
   );
